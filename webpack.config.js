b2d6e2ae97e9ad63988fab18f05e06f1f9a405c7
Joao Genio <joao.genio@teledyneflir.com>
2023-02-07 14:46:03 +0100

    converting to es mod

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
 const path = require('path');
+const { IgnorePlugin } = require('webpack');
 /* const webpack = require('webpack'); */
+
+const optionalPlugins = [];
+if (process.platform !== "darwin") { // don't ignore on OSX
+  optionalPlugins.push(new IgnorePlugin({ resourceRegExp: /^fsevents$/ }));
+}
 
 module.exports = {
   entry: [
-    path.resolve(__dirname,'bin/electron-mocha.js'),
+    /* path.resolve(__dirname,'bin/electron-mocha.js'), */
+    path.resolve(__dirname,'lib/main.js'),
   ],
+  target: 'node',
   mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist/'),
     filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js'/* , '.ts', '.tsx' */],
+    extensions: ['.js', '.json'/* , '.ts', '.tsx' */],
     fallback: {
         path: require.resolve("path-browserify"),
         fs: false,
-    }
+    },
   },
-  /* module: {
+  module: {
     rules: [
       {
+        test: /\.(txt)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      /* {
         test: /\.js$/,
         use: {
           loader: '@jsdevtools/coverage-istanbul-loader',
@@ -27,9 +43,9 @@ module.exports = {
         },
         enforce: 'post',
         exclude: /node_modules|lib|renderer|bin|\.spec\.js$/,
-      }
+      } */
     ]
-  }, */
+  },
   stats: {
     colors: true,
     modules: true,
@@ -37,6 +53,9 @@ module.exports = {
     errorDetails: true
   },
   devtool: 'inline-source-map',
+  plugins: [
+    ...optionalPlugins,
+  ],
   /* plugins: [
     new webpack.ProvidePlugin({
       Buffer: ['buffer', 'Buffer'],
